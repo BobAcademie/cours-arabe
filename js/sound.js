3413968cb629e845fa91b7f55ae0aeaf2fb319d4
BobAcademie <aboubakrhadid17@gmail.com>
2026-01-21 21:10:03 +0100

    Add files via upload

---
+++ js/sound.js
@@ -0,0 +1,11 @@
+const sounds = {
+  click: new Audio("../sounds/click.mp3"),
+  success: new Audio("../sounds/success.wav"),
+  error: new Audio("../sounds/error.wav"),
+};
+
+function playSound(name) {
+  if (!sounds[name]) return;
+  sounds[name].currentTime = 0;
+  sounds[name].play();
+}
